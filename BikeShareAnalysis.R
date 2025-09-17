8560a534e2667d7c933cbd3bb0597e22d0e90081
angelinacamen <impactgold42@yahoo.com>
2025-09-17 11:53:56 -0600

    Update BikeShareAnalysis.R

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -1,4 +1,6 @@
 library(tidymodels)
+install.packages("glmnet")
+
 
 bikeTrain <- vroom("~/Downloads/KaggleBikeShare/bike-sharing-demand/train.csv") 
 bikeTest <- vroom("~/Downloads/KaggleBikeShare/bike-sharing-demand/test.csv") #drop casual and registered variables
@@ -58,9 +60,48 @@ kaggle_submission_wf <- lin_preds %>%
 ## Write out the file
 vroom_write(x=kaggle_submission_wf, file="./BakedData.csv", delim=",")
 
+##Penalized Regression Model
+penalty_value <- 5      
+mixture_value <- 0.05 
+preg_model <- linear_reg(penalty=penalty_value, mixture=mixture_value) %>% #Set model and tuning
+  set_engine("glmnet") # Function to fit in R
 
 
+my_recipe <- recipe(count ~., data=bikeTrain) %>%
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>%
+  step_mutate(weather = factor(weather)) %>%
+  step_time(datetime, features=c("hour")) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_rm(datetime) %>%
+  step_zv(all_predictors()) %>%
+  step_lincomb(all_predictors()) %>%
+  step_normalize(all_numeric_predictors()) #Make mean 0, sd=1
 
+prepped_recipe <- prep(my_recipe)
+
+
+preg_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(lin_model) %>%
+  fit(data=bikeTrain)
+
+
+preds <- predict(preg_wf, new_data=bikeTest)
+preds <- preds %>%
+  mutate(.pred = exp(.pred))
+
+kaggle_submission <- bike_predictions %>%
+  bind_cols(., bikeTest) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+## Write out the file
+vroom_write(x=kaggle_submission, file="./PenalizedRegression1.csv", delim=",")
+
+
+##Linear Predictions
 linear_model <- linear_reg() %>% 
   set_engine("lm") %>% 
   set_mode("regression") %>% 
