36e4ed9bb4188f97984ce3d4828cbdd71ffc1f50
angelinacamen <impactgold42@yahoo.com>
2025-09-21 20:52:22 -0600

    Update BikeShareAnalysis.R

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -1,5 +1,10 @@
 library(tidymodels)
 install.packages("glmnet")
+install.packages("vroom")
+
+# Load the package
+library(vroom)
+
 
 
 bikeTrain <- vroom("~/Downloads/KaggleBikeShare/bike-sharing-demand/train.csv") 
@@ -61,8 +66,8 @@ kaggle_submission_wf <- lin_preds %>%
 vroom_write(x=kaggle_submission_wf, file="./BakedData.csv", delim=",")
 
 ##Penalized Regression Model
-penalty_value <- 5      
-mixture_value <- 0.05 
+penalty_value <- 1   
+mixture_value <- 0.01
 preg_model <- linear_reg(penalty=penalty_value, mixture=mixture_value) %>% #Set model and tuning
   set_engine("glmnet") # Function to fit in R
 
@@ -90,7 +95,7 @@ preds <- predict(preg_wf, new_data=bikeTest)
 preds <- preds %>%
   mutate(.pred = exp(.pred))
 
-kaggle_submission <- bike_predictions %>%
+kaggle_submission <- preds %>%
   bind_cols(., bikeTest) %>% #Bind predictions with test data
   select(datetime, .pred) %>% #Just keep datetime and prediction variables
   rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
@@ -98,7 +103,130 @@ kaggle_submission <- bike_predictions %>%
   mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
 
 ## Write out the file
-vroom_write(x=kaggle_submission, file="./PenalizedRegression1.csv", delim=",")
+vroom_write(x=kaggle_submission, file="./PenalizedRegression2.csv", delim=",")
+
+
+
+##Tuning and Fitting a Penalized Regression Model
+# Penalized regression model
+preg_model <- linear_reg(penalty=tune(), mixture=tune()) %>% #Set model and tuning
+  set_engine("glmnet") # Function to fit in R
+
+## Set Workflow
+preg_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(preg_model)
+
+## Grid of values to tune over
+grid_of_tuning_params <- grid_regular(penalty(),
+                                      mixture(),
+                                      levels = 6) ## L^2 total tuning possibilities
+
+## Split data for CV
+folds <- vfold_cv(bikeTrain, v = 6, repeats=1)
+
+# Run the CV
+CV_results <- preg_wf %>%
+  tune_grid(resamples=folds,
+          grid=grid_of_tuning_params,
+          metrics=metric_set(rmse, mae)) #Or leave metrics NULL
+
+## Plot Results 
+collect_metrics(CV_results) %>% # Gathers metrics into DF
+  filter(.metric=="rmse") %>%
+  ggplot(data=., aes(x=penalty, y=mean, color=factor(mixture))) +
+  geom_line()
+
+## Find Best Tuning Parameters
+bestTune <- CV_results %>%
+  select_best(metric="rmse")
+
+# Finalize the Workflow & fit it
+final_wf <- preg_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data=bikeTrain)
+
+## Predict
+final_preds <- final_wf %>%
+  predict(new_data = bikeTest)
+
+##Prepare for Kaggle Submission
+kaggle_submission <- final_preds %>%
+  bind_cols(., bikeTest) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+## Write out the file
+vroom_write(x=kaggle_submission, file="./TuningandFitting1.csv", delim=",")
+
+
+##Regression Trees
+install.packages("rpart")
+library(tidymodels)
+
+my_mod <- decision_tree(tree_depth = tune(),
+                        cost_complexity = tune(),
+                        min_n=tune()) %>% #Type of model
+  set_engine("rpart") %>% # What R function to use
+  set_mode("regression")
+
+## Create a workflow with model & recipe
+regtree_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod)
+
+## Grid of values to tune over
+grid_of_tuning_params <- grid_regular(
+  tree_depth(),
+  cost_complexity(),
+  min_n(),
+  levels = 3
+) 
+
+## Split data for CV
+folds <- vfold_cv(bikeTrain, v = 3, repeats = 1)
+
+# Run the CV
+CV_results <- regtree_wf %>%
+  tune_grid(
+    resamples = folds,
+    grid = grid_of_tuning_params,
+    metrics = metric_set(rmse, mae)
+  )
+
+# Plot results (use tree_depth or min_n instead of penalty/mixture)
+collect_metrics(CV_results) %>%
+  filter(.metric == "rmse") %>%
+  ggplot(aes(x = tree_depth, y = mean, color = factor(min_n))) +
+  geom_line()
+
+## Find Best Tuning Parameters
+bestTune <- CV_results %>%
+  select_best(metric = "rmse")
+
+# Finalize the workflow & fit it
+final_wf <- regtree_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = bikeTrain)
+
+
+## Predict
+final_preds <- final_wf %>%
+  predict(new_data = bikeTest)
+
+##Prepare for Kaggle Submission
+kaggle_submission <- final_preds %>%
+  bind_cols(., bikeTest) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+## Write out the file
+vroom_write(x=kaggle_submission, file="./RegressionTree.csv", delim=",")
+
 
 
 ##Linear Predictions
